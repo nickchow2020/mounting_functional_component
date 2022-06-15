7cd79b8474a61d66b6c00e3f43ac481ccc29d794
nick <smz1680@gmail.com>
2022-06-15 08:10:18 -0700

    add file

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyReactDOM from './myReact/MyReactDom';
+import './index.css';
+import App from './App';
+//import reportWebVitals from './reportWebVitals';
+
+// const reactE = <section>
+//   <header>Counter:0</header>
+//   <button>+</button><button>-</button>
+// </section>;
+
+const Button = (props) => {
+  <button>{props.children}</button>
+}
+
+class Counter extends React.Component {
+  // constructor(props) {
+  //   super(props)
+  // }
+
+  state = {
+    counter: 0,
+    title: "Counter"
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    // 
+    console.log('Props', props)
+    console.log('state', state)
+    if (props.title) {
+      return { ...state, title: props.title };
+    }
+    return state;
+  }
+
+  render() {
+    console.log("my button !!!",<Button />)
+    console.log("section", document.querySelector('section'))
+    console.log("STATE", this.state)
+    return <section>
+      <header>{this.state.title}:{this.state.counter}</header>
+      <Button>+</Button><Button>-</Button>
+    </section>;
+  }
+
+  componentDidMount() {
+    console.log("section", document.querySelector('section'))
+  }
+}
+
+// console.log("Counter", Counter)
+// console.log("<Counter/>", <Counter />)
+
+
+MyReactDOM.render(
+  <Counter title="My counter"/>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
---
+++ src/myReact/MyReactDom.js
@@ -0,0 +1,94 @@
+import React from 'react';
+
+const render = (
+    reactElement,
+    domElement
+) => {
+    let curDom;
+    console.log("TEST", reactElement)
+    if (reactElement === undefined) {
+        return;
+    }
+
+
+    if (typeof reactElement === 'string' | typeof reactElement === 'number') {
+        curDom =
+            document.createTextNode(reactElement);
+    } else {
+        const { type, props } = reactElement;
+        /// if type is ClassCompoennt
+        if (type.prototype instanceof React.Component) {
+            console.log('class componnent props', props)
+            /// Mounting
+            /// constructor
+            const curInstance = new type(props);
+            console.log("curInstance", curInstance)
+
+            // getDerivedStateFromProps
+            curInstance.state = type.getDerivedStateFromProps(props, curInstance.state)
+            console.log("curInstance", curInstance)
+
+            // render
+            const curReactElement = curInstance.render();
+            console.log("curReactElement", curReactElement);
+
+            render(curReactElement, domElement);
+            if (curInstance.componentDidmount) {
+                curInstance.componentDidmount()
+            }
+            return
+        }else if (typeof type === "function"){
+
+            // assignment to display functional component
+            curDom = document.createElement(type.name.toLowerCase())
+            curDom.textContent = props.children
+            console.log("it's a functional component",type.name.toLowerCase())
+        }else{
+            curDom = document.createElement(type);
+            Object.entries(props).forEach(
+                ([key, value]) => {
+                    if (key === 'children') {
+                        if (Array.isArray(value)) {
+                            console.log(value);
+                            value.forEach((rElement) => {
+                                render(rElement, curDom);
+                            });
+                        } else {
+                            render(value, curDom);
+                        }
+                    } else if (key.startsWith('on')) {
+                        curDom.addEventListener(
+                            getEventActionFromProps(key),
+                            value
+                        );
+                    } else {
+                        curDom[key] = value;
+                    }
+                }
+            );
+        }
+
+
+        console.log(curDom)
+        // Assignment if it is function component
+
+        /// else 
+
+    }
+
+    domElement.appendChild(curDom);
+};
+
+
+//utils
+
+function getEventActionFromProps(propsKey) {
+    return propsKey.slice(2).toLowerCase();
+}
+
+const MyReactDOM = {
+    render: render
+}
+
+
+export default MyReactDOM
